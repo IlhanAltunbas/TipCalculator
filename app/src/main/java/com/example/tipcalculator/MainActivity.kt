aedffb71d833e6b1a0483e158bf00f805da34b7a
ilhan <ilhanaltunbas.01@gmail.com>
2023-12-25 01:10:38 +0300

    Added split feature to the Tip Calculator mobile application.

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -1,22 +1,34 @@
 package com.example.tipcalculator
 
+import android.animation.ArgbEvaluator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.view.View
+import android.widget.CheckBox
+import android.widget.CompoundButton
 import android.widget.EditText
 import android.widget.SeekBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 
 private const val TAG = "MainActivity"
 private const val INITIAL_TIP_PERCENT = 15
+
 class MainActivity : AppCompatActivity() {
     private lateinit var etBaseAmount: EditText
     private lateinit var seekBarTip: SeekBar
     private lateinit var tvTipPercentLabel: TextView
     private lateinit var tvTipAmount: TextView
     private lateinit var tvTotalAmount: TextView
+    private lateinit var tvTipDescription: TextView
+    private lateinit var tvSplitAmount: TextView
+    private lateinit var tvSplitLabel: TextView
+    private lateinit var cbSplit: CheckBox
+    private lateinit var etSplitCount: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,33 +37,100 @@ class MainActivity : AppCompatActivity() {
         tvTipPercentLabel = findViewById(R.id.tvTipPercentLabel)
         tvTipAmount = findViewById(R.id.tvTipAmount)
         tvTotalAmount = findViewById(R.id.tvTotalAmount)
+        tvTipDescription = findViewById(R.id.tvTipDescription)
+        tvSplitAmount = findViewById(R.id.tvSplitAmount)
+        tvSplitLabel = findViewById(R.id.tvSplitLabel)
+        etSplitCount = findViewById(R.id.etSplitCount)
+        cbSplit = findViewById(R.id.cbSplit)
 
         seekBarTip.progress = INITIAL_TIP_PERCENT
         tvTipPercentLabel.text = "$INITIAL_TIP_PERCENT%"
-        seekBarTip.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+        updateTipDescription(INITIAL_TIP_PERCENT)
+        seekBarTip.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
                 Log.i(TAG, "onProgressChanged $p1")
                 tvTipPercentLabel.text = "$p1%"
                 computeTipAndTotal()
+                updateTipDescription(p1)
             }
 
-            override fun onStartTrackingTouch(p0: SeekBar?) {            }
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
 
             override fun onStopTrackingTouch(p0: SeekBar?) {}
 
         })
-        etBaseAmount.addTextChangedListener(object: TextWatcher {
+        etBaseAmount.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
             }
 
             override fun afterTextChanged(p0: Editable?) {
-                Log.i(TAG, "afterTextChaged $p0")
+                Log.i(TAG, "afterTextChanged $p0")
                 computeTipAndTotal()
             }
 
         })
+        cbSplit.setOnCheckedChangeListener(object : CompoundButton.OnCheckedChangeListener {
+            override fun onCheckedChanged(p0: CompoundButton?, p1: Boolean) {
+                if (p1) {
+                    etSplitCount.visibility = View.VISIBLE
+                    tvSplitLabel.visibility = View.VISIBLE
+                    tvSplitAmount.visibility = View.VISIBLE
+                } else {
+                    etSplitCount.visibility = View.GONE
+                    tvSplitLabel.visibility = View.GONE
+                    tvSplitAmount.visibility = View.GONE
+                }
+            }
+        })
+        etSplitCount.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                Log.i(TAG, "afterTextChanged $p0")
+                divideTotal()
+            }
+
+        })
+
+
+    }
+
+    private fun divideTotal() {
+        if (etSplitCount.text.isEmpty() || etBaseAmount.text.isEmpty()) {
+            tvSplitAmount.text = ""
+            return
+        }
+        //1. Get the value of the base and tip percent
+        val totalAmount = tvTotalAmount.text.toString().toDouble()
+        val splitCount = etSplitCount.text.toString().toDouble()
+        //2. Divide total
+        val totalSplitAmount = totalAmount / splitCount
+        //Update the UI
+        tvSplitAmount.text = "%.2f".format(totalSplitAmount)
+    }
+
+    private fun updateTipDescription(tipPercent: Int) {
+        val tipDescription = when (tipPercent) {
+            in 0..9 -> "Poor"
+            in 10..14 -> "Acceptable"
+            in 15..19 -> "Good"
+            in 20..24 -> "Great"
+            else -> "Amazing"
+        }
+        tvTipDescription.text = tipDescription
+
+        val color = ArgbEvaluator().evaluate(
+            tipPercent.toFloat() / seekBarTip.max,
+            ContextCompat.getColor(this, R.color.color_worst_tip),
+            ContextCompat.getColor(this, R.color.color_best_tip)
+        ) as Int
+        tvTipDescription.setTextColor(color)
     }
 
     private fun computeTipAndTotal() {
@@ -71,3 +150,4 @@ class MainActivity : AppCompatActivity() {
         tvTotalAmount.text = "%.2f".format(totalAmount)
     }
 }
+
